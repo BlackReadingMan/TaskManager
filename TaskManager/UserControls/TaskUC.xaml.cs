ad206f1b767f577945ff5f635a5e91bc31e9c648
BlackReadingMan <krolik.reaktivnyj@gmail.com>
2024-09-07 20:59:39 +0400

    Revert "."
    This reverts commit ffbc90bf0af47144f980fcf4301db50b39b3900a.

--- TaskManager/UserControls/TaskUC.xaml.cs
+++ TaskManager/UserControls/TaskUC.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Windows.Input;
+using TaskManager.DataOut;
 using TaskManager.DB;
+using TaskManager.DB.Enums;
 using TaskManager.DB.Models;
 using TaskManager.Windows.MainWindows;
+using Xceed.Document.NET;
 using Task = TaskManager.DB.Models.Task;
 
 namespace TaskManager.UserControls;
@@ -12,17 +15,33 @@ namespace TaskManager.UserControls;
 public sealed partial class TaskUc : ListedUc<Task>
 {
   private Observer? _observer;
+
   public TaskUc()
   {
     this.InitializeComponent();
   }
-  protected override async void UpdateData()
+
+  protected override async System.Threading.Tasks.Task UpdateData()
   {
-    this._observer = await DBAPI.IsUserObserveTask(this.CurrentClass, App.CurrentUser);
-    this.SubscribeButton.Content = this._observer is not null ? "Не отслеживать" : "Отслеживать";
-    this.Name.Content = this.CurrentClass.Name;
+    if (this.CurrentClass.Responsible.HasValue && this.CurrentClass.Responsible == App.CurrentUser.Id)
+    {
+      this.SubscribeButton.IsEnabled = false;
+      this.SubscribeButton.Content = "Отслеживать";
+    }
+    else
+    {
+      this.SubscribeButton.IsEnabled = true;
+      this._observer = await DBAPI.IsUserObserveTask(this.CurrentClass, App.CurrentUser);
+      this.SubscribeButton.Content = this._observer is not null ? "Не отслеживать" : "Отслеживать";
+    }
+    this.TaskName.Content = this.CurrentClass.Name;
     this.Description.Text = this.CurrentClass.Description;
     this.CreationTime.Content = this.CurrentClass.CreationTime;
+    this.Responsible.Content = this.CurrentClass.Responsible is null ? "Не назначен" : (await DBAPI.GetItem<User>(this.CurrentClass.Responsible.Value))?.Login;
+    this.DeadLine.Content = this.CurrentClass.Deadline;
+    this.Status.Content = this.CurrentClass.Status.EnumDescription();
+    this.Priority.Content = this.CurrentClass.Priority.EnumDescription();
+    this.SetStateChangerContent();
   }
 
   private void TaskUc_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -36,11 +55,49 @@ public sealed partial class TaskUc : ListedUc<Task>
     {
       await DBAPI.RemoveItem(this._observer);
       this.SubscribeButton.Content = "Отслеживать";
+      this._observer = null;
     }
     else
     {
-      await DBAPI.AddItem(new Observer { IdTask = this.CurrentClass.Id, IdUser = App.CurrentUser.Id });
+      this._observer = new Observer { IdTask = this.CurrentClass.Id, IdUser = App.CurrentUser.Id };
+      await DBAPI.AddItem(this._observer);
       this.SubscribeButton.Content = "Не отслеживать";
     }
+  }
+
+  private void SetStateChangerContent()
+  {
+    switch (this.CurrentClass.Status)
+    {
+      case TaskStatus.NotAcceptedForWork:
+        this.StateChanger.Content = "Взять в работу";
+        this.StateChanger.IsEnabled = !this.IsResponsible();
+        break;
+      case TaskStatus.TakenIntoWork:
+        this.StateChanger.Content = "Отправить на проверку";
+        this.StateChanger.IsEnabled = !this.IsResponsible();
+        break;
+      case TaskStatus.UnderReview:
+        this.StateChanger.Content = "Подтвердить выполнение";
+        this.StateChanger.IsEnabled = this.IsResponsible();
+        break;
+      case TaskStatus.Completed:
+        this.StateChanger.Content = "Завершена";
+        this.StateChanger.IsEnabled = false;
+        break;
+    }
+  }
+
+  private bool IsResponsible()
+  {
+    return this.CurrentClass.Responsible == App.CurrentUser.Id;
+  }
+  private async void StateChanger_Click(object sender, System.Windows.RoutedEventArgs e)
+  {
+    this.CurrentClass.Status++;
+    this.SetStateChangerContent();
+    await NotificationManager.NotifyUsersAsync(this.CurrentClass, App.CurrentUser);
+    await DBAPI.EditItem(this.CurrentClass);
+
   }
 }
--- TaskManager/ViewModels/DialogViewModels/AddTaskWindowViewModel.cs
+++ TaskManager/ViewModels/DialogViewModels/AddTaskWindowViewModel.cs
@@ -1,63 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
-using TaskManager.DB.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using TaskManager.DB;
+using TaskManager.DB.Enums;
+using TaskManager.Utilities;
 using TaskManager.ViewModels.BaseViewModels;
+using Task = TaskManager.DB.Models.Task;
 
 namespace TaskManager.ViewModels.DialogViewModels;
 
 internal class AddTaskWindowViewModel : DialogWindowViewModel<Task>
 {
-  private string? _name;
+  private string _name = string.Empty;
+
   public string Name
   {
     set => this._name = value;
   }
-  private string? _description;
+
+  private string _description = string.Empty;
+
   public string Description
   {
     set => this._description = value;
   }
-  private string? _deadLine;
-  public string DeadLine
+
+  private DateTime? _deadLine;
+
+  public DateTime? DeadLine
   {
     set => this._deadLine = value;
   }
-  private int? _selectedResponsible;
-  public int SelectedResponsible
+
+  private string? _selectedResponsible;
+
+  public string SelectedResponsible
   {
     set => this._selectedResponsible = value;
   }
+
   private int _selectedPriority = 0;
+
   public int SelectedPriority
   {
     get => this._selectedPriority;
     set => this._selectedPriority = value;
   }
-  public List<string> Responsible { get; private set; } = [];
+
+  private Dictionary<string, int> _responsibleDictionary = [];
+
+  private ObservableCollection<string> _responsible = [];
+
+  public ObservableCollection<string> Responsible
+  {
+    get => this._responsible;
+    private set
+    {
+      this._responsible = value;
+      this.OnPropertyChanged();
+    }
+  }
+
   public List<TaskPriority> Priority { get; private set; } =
   [
+    TaskPriority.NotSet,
     TaskPriority.Low,
+    TaskPriority.Medium,
     TaskPriority.High,
     TaskPriority.Critical
   ];
 
+  private ICommand? _loadCommand;
+  public ICommand LoadCommand => this._loadCommand ??= new RelayCommand(async f => await this.GetUsers());
+
+  private async System.Threading.Tasks.Task GetUsers()
+  {
+    this._responsibleDictionary = await DBAPI.GetUsersLogins();
+    this.Responsible = new ObservableCollection<string>(this._responsibleDictionary.Keys);
+    this.Responsible.Insert(0, "Не назначен");
+  }
+
   protected override void AddSubject()
   {
-    var task = new Task()
+    var task = new Task
     {
       Name = this._name,
       Description = this._description,
-      Deadline = DateOnly.Parse(this._deadLine),
-      Priority = this._selectedPriority,
-      Responsible = this._selectedResponsible,
+      Deadline = this._deadLine is null ? null : DateOnly.FromDateTime(this._deadLine.Value),
+      Priority = (TaskPriority)this._selectedPriority,
+      Responsible =
+        this._selectedResponsible is null ||
+        !this._responsibleDictionary.TryGetValue(this._selectedResponsible, out var value)
+          ? null
+          : value,
       Status = 0,
       CreationTime = DateOnly.FromDateTime(DateTime.Now)
     };
+    this.DialogResult = task;
   }
 
-  protected override bool CanExecute(object parameter)
+  protected override bool CanAddExecute(object parameter)
   {
-    return this._name is not null &&
-           this._description is not null;
+    return this._name != string.Empty && (this._deadLine is null || (this._deadLine.HasValue)) &&
+           this._description != string.Empty;
   }
 }
